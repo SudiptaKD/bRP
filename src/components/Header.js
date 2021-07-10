148e391e1f96dc1f400e6008f729043073c3b7da
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-07-11 01:46:44 +0600

    added animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,29 +3,33 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar,  Row, Nav } from 'react-bootstrap';
 import '../App.css'
 
+import Resume from '../files/resume.pdf'
+
 const Header = () => {
     return (
         <header>
-            <Navbar expand="lg" style={{minHeight:'90px', backgroundColor:'#2e2e35'}}
+            <Navbar id="home" expand="lg" style={{minHeight:'90px', backgroundColor:'#2e2e35', padding:'20px 10px 0px 10px'}}
              className=" justify-content-between" collapseOnSelect>
                 <Navbar.Brand style={{marginLeft:'30px',  color:'#dadada'}}>
                     <LinkContainer to="/">
                         <Row  >
-                        <h2 className='myFName'>Sudipta</h2>
-                        <h2 className='myMName'>Kumar</h2>
-                        <h2 className='myLName'>Dam</h2>
+                        <h1 className='myFName'>Sudipta</h1>
+                        <h1 className='myMName'>Kumar</h1>
+                        <h1 className='myLName'>Dam</h1>
                         </Row>
                     </LinkContainer>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
-                        <Nav className="mr-2">
-                            <Nav.Link style={{marginRight:'2px', marginLeft:'60px', color:'#dadada', fontSize:'18px'}} 
-                            href="#home">Home</Nav.Link>
-                            <Nav.Link style={{marginRight:'2px', marginLeft:'66px' , color:'#dadada', fontSize:'18px'}}
-                             href="#projects">Projects</Nav.Link>
-                            <Nav.Link style={{marginRight:'150px', marginLeft:'66px', color:'#dadada', fontSize:'18px'}}
-                             href="#resume">Resume</Nav.Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" style={{margin:'0px auto'}} />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end"  >
+                        <Nav style={{margin:'0px auto'}}>
+                            <Nav.Link id="n" style={{margin:'0px  auto', color:'#dadada', fontSize:'18px'}} 
+                            href="#home" className='home'>Home</Nav.Link>
+                            <Nav.Link id="n"  style={{margin:'0px  auto' , color:'#dadada', fontSize:'18px'}}
+                             href="#projects" className='projects'>Projects</Nav.Link>
+                            <Nav.Link id="n"  style={{margin:'0px auto', color:'#dadada', fontSize:'18px'}}
+                             href = {Resume} target = "_blank" className='resume'>Resume</Nav.Link>
+                             <Nav.Link id="n" target="_blank" style={{margin:'0px auto', color:'#dadada', fontSize:'18px'}}
+                             href="https://github.com/SudiptaKD" className='github'>Github</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
             </Navbar>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Row, Col, Image,Card } from 'react-bootstrap';
+import { Row, Col, Image,Card, Button } from 'react-bootstrap';
 
 // importing Images
-import Avatar from '../img/AvatarMaker.png'
+import Avatar from '../img/avatar.svg'
 import Js from "../img/js.svg"
 import Sass from "../img/sass.svg"
 import Bs from "../img/bootstrap.svg"
@@ -11,8 +11,11 @@ import HTML from "../img/html5.svg"
 import CSS from "../img/css3.svg"
 import RB from "../img/RB.svg"
 import MUi from "../img/material-ui.svg"
+import Git from "../img/git.svg"
 import Pokedex from "../img/pokedex.jpg"
 import ATLA from "../img/avatartla.jpg"
+
+import Resume from '../files/resume.pdf'
 
 
 
@@ -20,35 +23,56 @@ const Homepage = () => {
     return (
         <div>
             <Row style={{backgroundColor:'#2e2e35', 
-                minHeight:'550px', padding:'100px 30px 20px' }} >
+                minHeight:'550px', padding:'80px 30px 20px' }} >
                 <Col xs={12} sm={12} md={6} lg={4} xl={4} >
                    <Row>
-                       <h3 style={{ margin:'0px auto', color:'#dadada'}}>Front-end web developer</h3>
+                       <h3 className="frontend" style={{ margin:'50px auto 10px', color:'#dadada'}}>
+                           Front-end <span style={{color:'#e4aa48'}}>web</span> developer</h3>
                    </Row>
                    <Row>
-                      <strong  style={{ margin:'0px auto', color:'#dadada'}}> dfgd khfd sjfh ksjh  kjdshf kjsh  kjsh kjsh  kjshd skjh  skjshd kjsh sjhd kjhs s </strong>
+                      <strong className='frontend-text' style={{ margin:'10px auto', color:'#dadada', textAlign:'center'}}>
+                           Familiarizing myself with Web. Currently working on React JS skills as a newbie </strong>
+                   </Row>
+                   <br/>
+                   <Row >
+                        <Col sm={12} xl={12} >
+                            <Card className='p-1 rounded text-center p-3 ' 
+                            style={{ border: 'none', backgroundColor:'#2e2e35', margin:'0px auto' }}>
+                                <a className="button" href = {Resume} target = "_blank"><button style={{margin:'0px auto'}}>Resume</button> </a>
+                                </Card>    
+                        </Col>
+                        <Col className="contacts" style={{padding:'20px 10px', margin:'0px auto', display:'flex', justifyContent:'center'}} sm={12} xl ={12}>
+                            <a id="link" target="_blank" href="https://www.linkedin.com/in/sudiptaKD">
+                                <strong>Linkdin</strong></a>
+                            <a id="link" target="_blank" href="https://www.facebook.com/sudiptakd/">
+                                <strong>Facebook</strong></a>
+                            <a id="link" target="_blank" 
+                            href="https://mail.google.com/mail/u/0/?fs=1&to=sudiptakumardam@gmail.com&tf=cm">
+                                <strong>Gmail</strong></a>
+                        </Col>
                    </Row>
                 </Col>
                 <Col xs={12} sm={12} md={6} lg={4} xl={4}  >
                     <Card className='p-1 my-3 rounded text-center ' 
                         style={{ border: 'none', backgroundColor:'#2e2e35' }}>
-                        <Image src={Avatar} style={{width:'300px', height:'300px', margin:'0px auto'}} rounded />
+                        <Image className="avatar" src={Avatar} style={{width:'300px', height:'300px', margin:'0px auto'}} rounded />
                     </Card>
                 </Col>
                 <Col xs={12} sm={12} md={12} lg={4} xl={4}>
                 <Card className='p-1 my-3 rounded text-center p-3 ' style={{ border: 'none', backgroundColor:'#2e2e35', margin:'0px auto' }}>
                         <Card.Body>
-                            <Card.Title style={{color:'#e4aa48',fontSize:"20px"}}>Skills</Card.Title>
+                            <Card.Title className="skills" style={{color:'#e4aa48',fontSize:"22px"}}>Skills</Card.Title>
                             <Card.Text style={{color:'#dadada'}}>
-                                <Row xl={3} lg={3} md={2} sm={2} xs={2} >
-                                    <Col><Image src={HTML} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={CSS} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={Js} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={Bs} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={Sass} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={RJs} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={RB} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
-                                    <Col><Image src={MUi} style={{width:'50px', height:'50px', marginTop:'11px'}} rounded /></Col>
+                                <Row xl={3} lg={3} md={3} sm={3} xs={3} >
+                                    <Col className="skill1"><Image src={HTML} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill2"><Image src={CSS} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill3"><Image src={Js} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill4"><Image src={Bs} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill5"><Image src={Sass} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill6"><Image src={RJs} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill7"><Image src={RB} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill8"><Image src={MUi} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
+                                    <Col className="skill9"><Image src={Git} style={{width:'50px', height:'50px', marginTop:'13px'}} rounded /></Col>
                                 </Row>
                             </Card.Text>
                             </Card.Body>
@@ -70,8 +94,8 @@ const Homepage = () => {
                             </Card.Text>
                         </Card.Body>
                         <Card.Body>
-                            <Card.Link href="https://sudiptakd.github.io/Pokedex/">Live Page</Card.Link>
-                            <Card.Link href="https://github.com/SudiptaKD/Pokedex">Code</Card.Link>
+                            <Card.Link target="_blank" href="https://sudiptakd.github.io/Pokedex/">Live Page</Card.Link>
+                            <Card.Link target="_blank" href="https://github.com/SudiptaKD/Pokedex">Code</Card.Link>
                         </Card.Body>
                         </Card>
                 </Col>
@@ -87,8 +111,8 @@ const Homepage = () => {
                             </Card.Text>
                         </Card.Body>
                         <Card.Body>
-                            <Card.Link href="https://avatar-the-last-airbinder.netlify.app/">Live Page</Card.Link>
-                            <Card.Link href="https://github.com/SudiptaKD/Avatar-The-Last-Airbinder">Code</Card.Link>
+                            <Card.Link target="_blank" href="https://avatar-the-last-airbinder.netlify.app/">Live Page</Card.Link>
+                            <Card.Link target="_blank" href="https://github.com/SudiptaKD/Avatar-The-Last-Airbinder">Code</Card.Link>
                         </Card.Body>
                         </Card>
                 </Col>
